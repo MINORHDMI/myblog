88173f64ae790adeda08a5b272f4d4bf945cbdeb
HDMIMINOR <1019364668@qq.com>
2021-02-08 09:02:38 +0800

    修改

--- src/main/java/com/star/dao/UserDao.java
+++ src/main/java/com/star/dao/UserDao.java
@@ -6,11 +6,7 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 /**
- * @Description: 用户持久层接口
- * @Author: ONESTAR
- * @Date: Created in 11:11 2020/3/26
- * @QQ群: 530311074
- * @URL: https://onestar.newstar.net.cn/
+ * @description: 用户持久层接口
  */
 @Mapper
 @Repository
